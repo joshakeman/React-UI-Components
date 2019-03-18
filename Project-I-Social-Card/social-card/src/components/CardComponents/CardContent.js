d3624d89d36f88fd430b5f645f872212d2f5061c
Josh Akeman <joshakeman@gmail.com>
2019-03-18 18:55:59 -0400

    moved everything over because I forgot to fork

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return (
+        <div class="card-content">
+            <h2 class="card-content-header">Get started with React</h2>
+            <p class="card-content-p">React makes it painless to create ineractive UIs. Design simple views for each state in your application.</p>
+            <p class="web-address">reactjs.org</p>
+        </div>
+    )
+}
+
+export default CardContent;
---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div class="footer">
+            <i class="far fa-comments"></i>
+            <i class="fas fa-sync-alt"></i>
+            <i class="far fa-heart"></i>
+            <i class="far fa-envelope"></i>
+        </div>
+    )
+};
+
+export default Footer;
