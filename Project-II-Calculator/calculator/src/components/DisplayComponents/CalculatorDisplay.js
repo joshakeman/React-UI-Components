9f542da2a7795d8b030c1cbb346d116e37ef6403
Josh Akeman <joshakeman@gmail.com>
2019-03-19 16:39:42 -0400

    forgot to commit along the way again :/

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Display.css';
+
+function CalculatorDisplay () {
+    return(
+    
+    <div className='calc-display'><h2>0</h2></div>);
+}
+
+export default CalculatorDisplay
